bbb3cc05d7f2dfb2d55ff9d117b7ef7cfc3dfd6e
Ankur Deep Jaiswal <ankur@techgentsia.com>
2023-03-28 15:39:38 +0530

    Build CJS via Babel

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 'use strict';
 
-const EventEmitter = require('events');
-
-class RandomTimeInterval extends EventEmitter {
-  constructor (constInterval = 10, randomInterval = 5) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _events = require("events");
+class RandomTimeInterval extends _events.EventEmitter {
+  constructor(constInterval = 10, randomInterval = 5) {
     super();
     this._constInterval = constInterval;
     this._randomInterval = randomInterval;
     this._onTickHandler = this._onTick.bind(this);
     this._timeout = setTimeout(this._onTickHandler, this._calcTimeout());
   }
-
-  _calcTimeout () {
+  _calcTimeout() {
     const constInterval = this._constInterval;
     const randomInterval = Math.floor(Math.random() * this._randomInterval) + 1;
     return (constInterval + randomInterval) * 1000;
   }
-
-  _onTick () {
+  _onTick() {
     try {
       this.emit('tick');
     } catch (e) {
@@ -27,8 +28,7 @@ class RandomTimeInterval extends EventEmitter {
       this._timeout = setTimeout(this._onTickHandler, this._calcTimeout());
     }
   }
-
-  close () {
+  close() {
     if (this._timeout) {
       clearTimeout(this._timeout);
       this._timeout = null;
@@ -36,5 +36,5 @@ class RandomTimeInterval extends EventEmitter {
     this.removeAllListeners();
   }
 }
-
-module.exports = RandomTimeInterval;
+var _default = RandomTimeInterval;
+exports.default = _default;
